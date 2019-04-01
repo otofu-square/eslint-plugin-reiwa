7e20fbff2468529cde776ccbd5fdf9ef6a28c5d5
otofu-square <otofu.square@gmail.com>
2019-04-01 11:40:59 +0900

    :+1: implement no-heisei-in-variable-name rule

---
+++ src/__tests__/no-heisei-in-variable-name.test.ts
@@ -0,0 +1,26 @@
+import { noHeiseiInVariableName } from "../no-heisei-in-variable-name";
+import { RuleTester } from "eslint";
+
+const ruleTester = new RuleTester({
+  parser: "@typescript-eslint/parser"
+});
+
+const message = "Do not use heisei or 平成 in variable name, use new era.";
+
+ruleTester.run("no-heisei", noHeiseiInVariableName, {
+  valid: [],
+  invalid: [
+    {
+      code: "const theBeginningOfHeisei = 1989",
+      errors: [{ message }]
+    },
+    {
+      code: "let heisei;",
+      errors: [{ message }]
+    },
+    {
+      code: "var 平成;",
+      errors: [{ message }]
+    }
+  ]
+});
---
+++ src/no-heisei-in-variable-name.ts
@@ -0,0 +1,26 @@
+import { Rule } from "eslint";
+
+export const noHeiseiInVariableName: Rule.RuleModule = {
+  meta: {
+    docs: {
+      description: "",
+      category: "Possible Errors",
+      recommended: true,
+      url: ""
+    },
+    schema: []
+  },
+
+  create: context => ({
+    VariableDeclarator: node => {
+      if (node.type !== "VariableDeclarator") return;
+      if (node.id.type !== "Identifier") return;
+      if (node.id.name.match(/平成|heisei/i)) {
+        context.report({
+          node,
+          message: "Do not use heisei or 平成 in variable name, use new era."
+        });
+      }
+    }
+  })
+};
